995442b3a22bc45b60cb75304e65ed6f35be2cd5
john lundgren <john.lundgren.lexicon@gmail.com>
2021-05-30 09:29:07 +0200

    Added static welcome page.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const usersRouter = require('./routers/usersRouter');
 
 const app = express();
 
+app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
