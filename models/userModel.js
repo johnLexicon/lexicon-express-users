bc2231b942b9d4df2c45bfc42fdd0f936a029b43
John Lundgren <john.lundgren.lexicon@gmail.com>
2021-05-30 14:24:02 +0200

    Added name to user model.

--- models/userModel.js
+++ models/userModel.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema(
   {
+    name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     hashedPassword: { type: String, required: true, select: false },
     avatar: { type: String },
